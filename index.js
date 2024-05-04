ae670a4a009b0d7c3d2f70154645567fffb2742e
sysc-rem <raipam44@gmail.com>
2024-05-04 13:29:07 +0800

    start

---
+++ index.js
@@ -0,0 +1,33 @@
+const display = document.getElementById("display");
+window.addEventListener("keydown", handleKeyPress);
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function deleteFromDisplay() {
+  display.value = display.value.slice(0, -1);
+}
+
+function clearDisplay(input) {
+  display.value = "";
+}
+
+function handleKeyPress(event) {
+  const key = event.key;
+  const allowedInputs = /[0-9+\-*/().]/;
+
+  if (event.key === "Backspace") {
+    deleteFromDisplay();
+  } else if (allowedInputs.test(key)) {
+    appendToDisplay(key);
+  }
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
